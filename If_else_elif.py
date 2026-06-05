02f194e8a03eb69fda795fa7be5a7ad8dc6519cb
haranathx <haranathx@gmail.com>
2026-06-05 22:37:59 +0530

    PP

---
+++ If_else_elif.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+# if name == "Hara":
+#     print("Hello, Hara!")
+
+
+# if name == "Hara":
+#     print("Hello, Hara!")
+# else:    
+#     print("Hello, stranger!")
+
+
+if name == "Hara":
+    print("Hello, Hara!")
+elif name == "Alice":
+    print("Hello, Alice!")
+else:
+    print("Hello, stranger!")
